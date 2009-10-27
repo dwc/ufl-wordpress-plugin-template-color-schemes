ae7e4a84f13276de21675cba7b431fda47378aaf
danieltwc <danieltwc@427a1fce-c2fd-11de-a759-a54c9d3330c2>
2009-10-27 15:05:51 +0000

    Reorganize repository to follow standard layout

---
+++ plugins/class.UfTemplateColorSchemesPlugin.php
@@ -0,0 +1,25 @@
+<?php
+require_once(UF_PLUGIN_FRAMEWORK_LIBRARY . '/class.UfImageRadioOption.php');
+require_once(UF_PLUGIN_FRAMEWORK_LIBRARY . '/class.UfOptionGroup.php');
+require_once(UF_PLUGIN_FRAMEWORK_LIBRARY . '/class.UfOptionsPage.php');
+require_once(UF_PLUGIN_FRAMEWORK_LIBRARY . '/class.UfPlugin.php');
+
+if (! class_exists('UfTemplateColorSchemesPlugin')) {
+	class UfTemplateColorSchemesPlugin extends UfPlugin {
+		function UfTemplateColorSchemesPlugin($name, $file) {
+		    $source = UF_TEMPLATE_COLOR_SCHEMES_PLUGIN_URL . '/screenshots/';
+		    $radioOptions = array();
+
+                    for($option = 1; $option <= 8; $option++) {
+		        $radioOptions[$option] = new UfImageRadioOption('uf_template_color_scheme', '0' . $option . 'a', 'Color Scheme ' . $option, $source . '0' . $option . 'a.jpg');
+		    }
+
+		    $options = array(new UfOptionGroup('Template A', $radioOptions));
+
+		    $this->add_admin_page(new UfOptionsPage($name, '', $options));
+
+		    $this->{get_parent_class(__CLASS__)}($name, $file);
+		}
+	}
+}
+?>
---
+++ uf-template-color-schemes.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: UF Template Color Schemes
+Version: 0.1
+Plugin URI: http://www.webadmin.ufl.edu/
+Description: Color scheme selector for WordPress themes based on University of Florida <a href="http://www.webadmin.ufl.edu/templates">Templates</a>.
+Author: Joey Spooner <spooner@ufl.edu>
+Author URI: http://www.spoonstein.com/
+*/
+
+define('UF_TEMPLATE_COLOR_SCHEMES_PLUGIN_BASE', dirname(__FILE__) . '/');
+define('UF_TEMPLATE_COLOR_SCHEMES_PLUGIN_URL', WP_PLUGIN_URL . '/' . str_replace(basename(__FILE__), "", plugin_basename(__FILE__)));
+
+// Load the plugin after the framework
+add_action('plugins_loaded', 'uf_template_color_schemes_plugins_loaded');
+
+$uf_template_color_schemes_plugin = null;
+function uf_template_color_schemes_plugins_loaded() {
+	global $uf_template_color_schemes_plugin;
+
+	require_once('plugins/class.UfTemplateColorSchemesPlugin.php');
+	$uf_template_color_schemes_plugin = new UfTemplateColorSchemesPlugin('Color Schemes', __FILE__);
+}
+
+function uf_template_color_scheme() {
+	$color_scheme = get_option('uf_template_color_scheme'); 
+
+	if($color_scheme) {
+	    return $color_scheme;
+	}
+}
+?>
